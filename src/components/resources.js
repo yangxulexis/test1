6b8dbd7fd580150d4018f43fae7a5f609d03e419
ni san <xydotguy@gmail.com>
2017-10-20 01:34:36 -0400

    first commit

---
+++ src/components/resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Special recipe
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+            </ul>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
+import Resources from './components/resources';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/resources" component={Resources} />
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
